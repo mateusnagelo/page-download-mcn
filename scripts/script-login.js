9d0c016183832b6bb4b4d23ea97ef9d3af4bfa6e
mateusnagelo <mateusgenius1234@hotmail.com>
2022-07-08 17:25:12 -0300

    ajuste no js

--- scripts/script-login.js
+++ scripts/script-login.js
@@ -1,7 +1,8 @@
+
 function entrar(e) {
 
     const email = 'mateusangelo@hotmail.com';
-    const password = '123456';
+    const password = '0';
 
     const id = (elem) => {
        return document.getElementById(elem);
@@ -34,10 +35,9 @@ function entrar(e) {
     }
 
 
-    
-document.addEventListener('keyup', function(e){
-    if(e.key === 'Enter' || e.key === 'NumpadEnter'){
-        entrar();
+document.addEventListener('keypress', function(e){
+    if(e.key == 'Enter'){
+        return entrar();
     }
 });
 
@@ -45,6 +45,69 @@ document.addEventListener('keyup', function(e){
 let acessarIndex = id('button');
 
 acessarIndex.addEventListener('click', entrar, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function entrar(e) {
+
+//     const email = 'mateusangelo@hotmail.com';
+//     const password = '123456';
+
+//     const id = (elem) => {
+//        return document.getElementById(elem);
+//     }
+
+//     const validEmail = id('email').value;
+//     const validPassword = id('password').value;
+
+//     if(validEmail !== email && validPassword !== password){
+//         alert('Preencha os dados corretamente!')
+//     }else if(validEmail === email && validPassword !== password){
+//         alert('Senha incorreta!')
+//         id('password').style.borderBottom = '1px solid #FF0000';
+//         return e.preventDefault();
+//     }else if(validEmail !== email && validPassword === password){
+//         alert('E-mail incorreto!')
+//         id('email').style.borderBottom = '1px solid #FF0000';
+//         return e.preventDefault();
+//     }
+//     else{
+//         return true;
+//     }
+// }
+
+//     const id = (elem) => {
+//        return document.getElementById(elem);
+//     }
+
+
+    
+// document.addEventListener('keyup', function(e){
+//     if(e.key === 'Enter' || e.key === 'NumpadEnter'){
+//         entrar();
+//     }
+// });
+
+
+// let acessarIndex = id('button');
+
+// acessarIndex.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     entrar
+// }, false);
 
 
 
