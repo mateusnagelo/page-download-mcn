d41a0b3c43732dede46e7423740869dbfca4ad2e
mateusnagelo <mateusgenius1234@hotmail.com>
2022-07-08 11:32:06 -0300

    teste

---
+++ scripts/script-login.js
@@ -0,0 +1,50 @@
+function entrar(e) {
+
+    const email = 'mateusangelo@hotmail.com';
+    const password = '123456';
+
+    const id = (elem) => {
+       return document.getElementById(elem);
+    }
+
+    const validEmail = id('email').value;
+    const validPassword = id('password').value;
+
+    if(validEmail === email && validPassword === password){
+        return true;
+    }else if(validEmail === email && validPassword !== password){
+        alert('Senha incorreta!')
+        id('password').style.borderBottom = '1px solid #FF0000';
+        return e.preventDefault();
+    }else if(validEmail !== email && validPassword === password){
+        alert('E-mail incorreto!')
+        id('email').style.borderBottom = '1px solid #FF0000';
+        return e.preventDefault();
+    }
+    else{
+        id('password').style.borderBottom = '1px solid #FF0000';
+        id('email').style.borderBottom = '1px solid #FF0000';
+        alert('Preencha os dados corretamente!')
+        return e.preventDefault();
+    }
+}
+
+    const id = (elem) => {
+       return document.getElementById(elem);
+    }
+
+
+    
+document.addEventListener('keyup', function(e){
+    if(e.key === 'Enter' || e.key === 'NumpadEnter'){
+        entrar();
+    }
+});
+
+let acessarIndex = id('button');
+
+acessarIndex.addEventListener('click', entrar, false);
+
+
+
+
