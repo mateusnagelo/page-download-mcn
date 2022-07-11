a72618c78e4212f6a9c6a3535191758be0931815
mateusnagelo <mateusgenius1234@hotmail.com>
2022-07-11 10:59:33 -0300

    alteração no  alert

--- scripts/script-login.js
+++ scripts/script-login.js
@@ -18,7 +18,7 @@ function entrar(e) {
         id('password').style.borderBottom = '1px solid #FF0000';
         return e.preventDefault();
     }else if(validEmail !== email && validPassword === password){
-        alert('E-mail incorreto!')
+        alert('E-mail ou usuario incorreto!')
         id('email').style.borderBottom = '1px solid #FF0000';
         return e.preventDefault();
     }
